b6554d27fcb525b4d76c9fd4861deca7ffbe6fe8
temeculous <zacerious@gmail.com>
2023-07-23 18:12:32 -0400

    stubbed up note routes and controller

---
+++ controllers/notes.js
@@ -0,0 +1,8 @@
+module.exports = {
+  show,
+  create,
+};
+
+function create(req, res) {}
+
+function show(req, res) {}
---
+++ routes/notes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const noteCtrl = require("../controllers/notes");
+
+//get show page
+//post new note
